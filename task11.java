28472cdb688692ab95a9ab52c53da3f435479efc
SirApoLLon <salo.kx@mail.ru>
2023-10-01 22:29:45 +0300

    дз 11

---
+++ task11.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class task11 {
+    public static void task11 (String[] args){
+        Scanner skanner = new Scanner(System.in);
+        int a,b;
+        a = skanner.nextInt();
+        b = skanner.nextInt();
+
+        System.out.println(a+b);
+    }
+}
